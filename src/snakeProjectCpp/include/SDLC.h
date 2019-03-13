6b74e8860377d1b61fc7f2e2110014dbbf475a53
Weltgeist <narcisse118@gmail.com>
2019-03-13 00:11:40 -0400

    Created SDLC.cpp-Moved From Main-Modularity

--- src/snakeProjectCpp/include/SDLC.h
+++ src/snakeProjectCpp/include/SDLC.h
@@ -1,16 +1,18 @@
 #ifndef SDLC_H
 #define SDLC_H
-
+#include <string>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "InitError.h"
 
 class SDLC
 {
-    public:
-        SDLC();
-        virtual ~SDLC();
-
-    protected:
-
-    private:
+    SDL_Window * m_window;
+    SDL_Renderer * m_renderer;
+public:
+    SDLC( Uint32 flags = 0 );
+    virtual ~SDLC();
+    void draw();
 };
 
 #endif // SDLC_H

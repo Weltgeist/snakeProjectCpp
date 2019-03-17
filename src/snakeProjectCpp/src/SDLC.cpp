c0dd6c4464ad3628a2ca8f2d60e4f017d5bf2dee
Weltgeist <narcisse118@gmail.com>
2019-03-16 21:44:03 -0400

    codeblock conf updates, fixed SDLC.cpp to shows lines on app

--- src/snakeProjectCpp/src/SDLC.cpp
+++ src/snakeProjectCpp/src/SDLC.cpp
@@ -13,20 +13,20 @@ SDLC::SDLC( Uint32 flags )
 
     //if ( SDL_CreateWindowAndRenderer( SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN, &m_window, &m_renderer ) != 0 ) //Combination of SDLCreateWindow & SDL_CreateRenderer
     m_window =SDL_CreateWindow("Weltgeist's Snake Application",  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if ( m_window != 0 )
+    if ( m_window == nullptr ) // Follow C++11 std
         throw InitError();
     m_renderer=SDL_CreateRenderer(m_window,-1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-    if ( m_renderer != 0 )
+    if ( m_renderer == nullptr )
         throw InitError();
     std::string imagePath = getResourcePath() + "ok.bmp";
     m_bmp=SDL_LoadBMP(imagePath.c_str());
-    if ( m_renderer != 0 )
+    if ( m_renderer == nullptr )
         throw InitError();
 
     SDL_SetWindowIcon(m_window,m_bmp);
 
     m_tex=SDL_CreateTextureFromSurface(m_renderer,m_bmp );
-    if ( m_renderer != 0 )
+    if ( m_renderer == nullptr )
         throw InitError();
 }
 
@@ -66,9 +66,10 @@ void SDLC::draw() //redraw
     {
         //SDL_SetRenderDrawColor( m_renderer, rgb[i], rgb[i + 1], rgb[i + 2], 255 );
         //SDL_RenderFillRect( m_renderer, &colorBar );
-        //SDL_RenderPresent( m_renderer );
+        SDL_RenderPresent( m_renderer );
         SDL_Delay( 500 );
     }
+
 }
 
 void SDLC::drawGrid()
@@ -76,6 +77,7 @@ void SDLC::drawGrid()
     unsigned int sizeBetween=SCREEN_WIDTH/SCREEN_ROWS;
     unsigned int x=0;
     unsigned int y=0;
+    SDL_SetRenderDrawColor(m_renderer, 255, 255, 255, 255);
         for(unsigned int i=0;i<SCREEN_ROWS;i++){
             x+=sizeBetween;
             y+=sizeBetween;
